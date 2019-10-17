987352b895ab3e581f1d38f93f7e09df40a75a37
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-10-17 18:58:14 +0700

    Add files via upload

---
+++ PBO2-10118085-Latihan40-WarnaKepribadian/src/pbo2/pkg10118085/latihan40/warnakepribadian/User.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan40.warnakepribadian;
+
+/**
+ *
+ * @author Risky
+ * Nama     : Risky Irfansyah
+ * Kelas    : PBO2
+ * Nim      : 10118085
+ * Deskripsi Tugas : Warna Kepribadian
+ */
+public class User {
+    private String nama;
+    private String warna;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+    
+    
+}
